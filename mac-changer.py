77aa0db3b92c49179ffc3c5bce1df76c97c91da4
b-giblin <bgiblin893@gmail.com>
2023-09-05 12:08:08 -0400

    Basic refactoring and function use

--- mac-changer.py
+++ mac-changer.py
@@ -29,12 +29,20 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-options = get_arguments()
-# change_mac(options.interface, options.new_mac)
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
 
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
-mac_address_search_result = re.search(
-    r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result
-).group()
-print(mac_address_search_result)
+    mac_address_search_result = re.search(
+        r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result
+    ).group()
+
+    if mac_address_search_result:
+        return mac_address_search_result
+    else:
+        print("Error, Could not find valid MAC Address")
+
+
+options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = {}").format(current_mac)
+# change_mac(options.interface, options.new_mac)
